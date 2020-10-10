198714b944a98437bc6f597b1575352a05a027d5
Jordan C <9444763+jordancheval@users.noreply.github.com>
2020-10-10 10:03:53 +0200

    🎨 : ajouts pour le style

--- layouts/fiche.11ty.js
+++ layouts/fiche.11ty.js
@@ -11,16 +11,18 @@ class Fiche {
     return `
     <h1>${champs.nom}</h1>
     <div class="bloc-info">
-      <span>Adresse</span>
+      <span class="titre">Adresse</span>
       <span>${champs.adresse}</span>
     </div>
     <div class="bloc-info">
-      <span>${champs.typesDeSurfaces.length > 1 ? 'Types de surfaces' : 'Type de surface'}</span>
+      <span class="titre">${champs.typesDeSurfaces.length > 1 ? 'Types de surfaces' : 'Type de surface'}</span>
       <span>${champs.typesDeSurfaces.join(' - ')}</span>
     </div>
-    <a href="https://maps.google.com/maps?q=loc:${champs.localisation.lat},${champs.localisation.lon}&z=17&t=h&hl=fr-FR&gl=US&mapclient=embed">Ouvrir sur Maps</a>
+    <div>
+      <a href="https://maps.google.com/maps?q=loc:${champs.localisation.lat},${champs.localisation.lon}&z=17&t=h&hl=fr-FR&gl=US&mapclient=embed" target="_blank">Ouvrir sur Maps</a>
+    </div>
     <div class="carte">
-      CARTE
+      <iframe src="https://maps.google.com/maps?q=loc:${champs.localisation.lat},${champs.localisation.lon}&z=17&t=h&output=embed" frameborder="0"></iframe>
     </div>
     <link rel="stylesheet" href="${this.url('/css/fiche.css')}">
     `
