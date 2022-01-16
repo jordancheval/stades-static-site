54ab5fc9a99d9dc5969000310c788f7c02fd6dee
Jordan C <9444763+jordancheval@users.noreply.github.com>
2022-01-16 14:14:45 +0100

    ⚙ : montée de version + fix de sécurité

---
+++ workbox-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  globDirectory: 'public/',
+  globPatterns: [
+    '**/*.{html,css,js,mjs,map,jpg,png,gif,webp,ico,svg,woff2,woff,eot,ttf,otf,ttc,json}'
+  ],
+  skipWaiting: true,
+  clientsClaim: true,
+  swDest: 'public/service-worker.js',
+  ignoreURLParametersMatching: [
+    /^utm_/,
+    /^fbclid$/
+  ],
+  navigateFallback: '404.html',
+  runtimeCaching: [
+    {
+      urlPattern: /^.*\.(html|jpg|png|gif|webp|ico|svg|woff2|woff|eot|ttf|otf|ttc|json)?$/,
+      handler: `StaleWhileRevalidate`
+    },
+    {
+      urlPattern: /^.*\/$/,
+      handler: `StaleWhileRevalidate`
+    }
+  ]
+}
