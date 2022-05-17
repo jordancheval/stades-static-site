588a22fe12b06c127afffb36d921bbbfbe118265
Jordan C <9444763+jordancheval@users.noreply.github.com>
2022-05-17 20:00:02 +0200

    ✨ recherche avec flèches ⬆ et ⬇

--- src/index.md
+++ src/index.md
@@ -5,7 +5,7 @@ titre: 'Accueil'
 
 # Je cherche le stade
 
-<input type="text" id="recherche" placeholder="Tapez le nom d'un stade, d'un club, d'une ville..." aria-label="Champ de recherche">
+<input type="text" id="recherche" placeholder="Tapez le nom d'un stade, d'un club, d'une ville..." aria-label="Champ de recherche" autocomplete="off" autofocus>
 
 <div class="liens">
   <a href="./cookies.html">Gérer les cookies</a>
--- src/ressources/js/recherche.js
+++ src/ressources/js/recherche.js
@@ -6,13 +6,15 @@ class Recherche {
    */
   constructor(input) {
     if (window.Worker) {
+      this.input = input
       this.worker = new Worker(`${window.location.origin}/js/worker.js`)
       this.conteneurResultat = this.creerConteneurResultats()
 
-      input.insertAdjacentElement('afterend', this.conteneurResultat)
+      this.input.insertAdjacentElement('afterend', this.conteneurResultat)
 
-      this.preparerInputPourEnvoiDeMessages(input)
+      this.preparerInputPourEnvoiDeMessages()
       this.preparerRecepetionMessagesWorker(this.worker)
+      this.gererFlechesClavier()
     } else {
       console.error('Worker non supporté')
     }
@@ -20,18 +22,17 @@ class Recherche {
 
   /**
    * Ajoute la fonction d'envoi de message sur l'élément input
-   * @param {HTMLInputElement} input - l'élément de saisie de texte
    */
-  preparerInputPourEnvoiDeMessages(input) {
-    input.onkeydown = (evenement) => {
+  preparerInputPourEnvoiDeMessages() {
+    this.input.onkeydown = (evenement) => {
       if (!evenement.key.match(new RegExp('[\\w\\d\\u00C0-\\u00FF\\.-\\s]'))) {
         evenement.preventDefault()
       }
     }
 
-    input.oninput = () => {
-      if (input.value.length > 2) {
-        this.worker.postMessage(input.value)
+    this.input.oninput = () => {
+      if (this.input.value.length > 2) {
+        this.worker.postMessage(this.input.value)
       } else {
         this.conteneurResultat.innerHTML = ''
       }
@@ -44,7 +45,7 @@ class Recherche {
    */
   preparerRecepetionMessagesWorker(worker) {
     worker.onmessage = (message) => {
-      const html = message.data.length > 0 ? `${message.data.map(({slug, nom}) => `<a href="/stades/${slug}/">${nom}</a>`).join('')}` : '<span>Aucun résultat</span>'
+      const html = message.data.length > 0 ? `${message.data.map(({slug, nom}) => `<a href="/stades/${slug}/" tabindex="-1">${nom}</a>`).join('')}` : '<span>Aucun résultat</span>'
 
       this.conteneurResultat.innerHTML = ''
 
@@ -62,5 +63,48 @@ class Recherche {
     div.id = 'resulats-recherche'
 
     return div
+  }
+
+  /**
+   * Permet de gérer la sélection du stade avec les flèches "haut" et "bas" du clavier.
+   */
+  gererFlechesClavier() {
+    const fonctionNavigationClavier = (evenement) => {
+      const elementsFocusable = this.conteneurResultat.querySelectorAll('a')
+
+      if (['ArrowDown', 'ArrowUp'].includes(evenement.key) && elementsFocusable.length > 0) {
+        const elementAyantLeFocus = [...elementsFocusable].find((element) => element.matches(':focus'))
+
+        evenement.preventDefault()
+
+        if (!elementAyantLeFocus) {
+          elementsFocusable[evenement.key === 'ArrowDown' ? 0 : elementsFocusable.length - 1].focus()
+        } else {
+          const elementOuVaLeFocus = [
+            {
+              test: evenement.key === 'ArrowDown' && elementAyantLeFocus !== [...elementsFocusable].slice(-1)[0],
+              element: () => elementAyantLeFocus.nextElementSibling
+            },
+            {
+              test: evenement.key === 'ArrowDown' && elementAyantLeFocus === [...elementsFocusable].slice(-1)[0],
+              element: () => elementsFocusable[0]
+            },
+            {
+              test: evenement.key === 'ArrowUp' && elementAyantLeFocus !== elementsFocusable[0],
+              element: () => elementAyantLeFocus.previousElementSibling
+            },
+            {
+              test: evenement.key === 'ArrowUp' && elementAyantLeFocus === elementsFocusable[0],
+              element: () => [...elementsFocusable].slice(-1)[0]
+            }
+          ]
+
+          elementOuVaLeFocus.find(({test}) => test)?.element().focus()
+        }
+      }
+    }
+
+    this.input.addEventListener('keydown', fonctionNavigationClavier)
+    this.conteneurResultat.addEventListener('keydown', fonctionNavigationClavier)
   }
 }
